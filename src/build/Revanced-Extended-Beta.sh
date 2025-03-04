852d1246b3b3c4ae03c3d4728e2b59d178edbfab
Fioren <102145692+FiorenMas@users.noreply.github.com>
2025-03-04 23:35:45 +0700

    rvx

--- src/build/Revanced-Extended-Beta.sh
+++ src/build/Revanced-Extended-Beta.sh
@@ -6,14 +6,15 @@ source src/build/utils.sh
 dl_gh "revanced-patches revanced-cli" "inotia00" "prerelease"
 
 # Patch YouTube:
-#get_patches_key "youtube-revanced-extended"
-#get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+get_patches_key "youtube-revanced-extended"
+version="20.06.42"
+get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 #split_editor "youtube-beta" "youtube-beta"
 #patch "youtube-beta" "revanced-extended" "inotia"
 # Patch Youtube Arm64-v8a
 #get_patches_key "youtube-revanced-extended"
-#split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
-#patch "youtube-beta-arm64-v8a" "revanced-extended" "inotia"
+split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+patch "youtube-beta-arm64-v8a" "revanced-extended" "inotia"
 # Patch Youtube Armeabi-v7a
 #get_patches_key "youtube-revanced-extended"
 #split_editor "youtube-beta" "youtube-beta-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
@@ -27,35 +28,36 @@ dl_gh "revanced-patches revanced-cli" "inotia00" "prerelease"
 #split_editor "youtube-beta" "youtube-beta-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
 #patch "youtube-beta-x86_64" "revanced-extended" "inotia"
 # Patch YouTube:
-get_patches_key "youtube-revanced-extended"
-get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
-patch "youtube-beta" "revanced-extended" "inotia"
+# get_patches_key "youtube-revanced-extended"
+# get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
+# patch "youtube-beta" "revanced-extended" "inotia"
 # Split architecture Youtube:
-get_patches_key "youtube-revanced-extended"
-for i in {0..3}; do
-  split_arch "youtube-beta" "revanced-extended" "$(gen_rip_libs ${libs[i]})"
-done
+# get_patches_key "youtube-revanced-extended"
+# for i in {0..3}; do
+#   split_arch "youtube-beta" "revanced-extended" "$(gen_rip_libs ${libs[i]})"
+# done
 
 # Patch YouTube Music Extended:
 # Arm64-v8a
 get_patches_key "youtube-music-revanced-extended"
+version="8.05.51"
 get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-beta-music-arm64-v8a" "revanced-extended" "inotia"
 # Armeabi-v7a
-get_patches_key "youtube-music-revanced-extended"
-version="8.02.52"
-get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
-patch "youtube-beta-music-armeabi-v7a" "revanced-extended" "inotia"
+# get_patches_key "youtube-music-revanced-extended"
+# version="8.02.52"
+# get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
+# patch "youtube-beta-music-armeabi-v7a" "revanced-extended" "inotia"
 
 # Patch Reddit:
-get_patches_key "reddit-rve"
-get_apk "com.reddit.frontpage" "reddit-beta" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
-split_editor "reddit-beta" "reddit"
-patch "reddit-beta" "revanced-extended" "inotia"
+# get_patches_key "reddit-rve"
+# get_apk "com.reddit.frontpage" "reddit-beta" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
+# split_editor "reddit-beta" "reddit"
+# patch "reddit-beta" "revanced-extended" "inotia"
 # Patch Arm64-v8a:
-split_editor "reddit-beta" "reddit-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-get_patches_key "reddit-rve"
-patch "reddit-arm64-v8a-beta" "revanced-extended" "inotia"
+# split_editor "reddit-beta" "reddit-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+# get_patches_key "reddit-rve"
+# patch "reddit-arm64-v8a-beta" "revanced-extended" "inotia"
 
 # Patch YouTube Lite Arm64-v8a:
 #get_patches_key "youtube-revanced-extended"
