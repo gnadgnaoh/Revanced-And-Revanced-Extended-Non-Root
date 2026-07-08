b61243e6233d526179412010b0289c77d3217d3f
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-07-08 10:27:48 +0700

    Update Xposed-Beta.sh

--- src/build/Xposed-Beta.sh
+++ src/build/Xposed-Beta.sh
@@ -3,7 +3,8 @@
 source ./src/build/utils.sh
 
 NPatch_dl(){
-	telegram_dl "https://t.me/onpatch" "10" "*.jar" "jar-npatch.jar"
+	telegram_dl "https://t.me/onpatch" "10" "*.jar" "jar-npatch.jar" || return 1
+	mv -f "./download/jar-npatch.jar" "./jar-npatch.jar"
 }
 patch_dl(){
 	dl_gh "NexAlloy" "gnadgnaoh" "latest"
@@ -20,20 +21,20 @@ patch_dl(){
 	patch_dl
 	# Patch Facebook:
 	get_apk "com.facebook.katana" "facebook-beta-arm64-v8a" "bundle" "arm64-v8a" " nodpi" "Android 11+"
-	npatch "facebook-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "facebook-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" " --sigbypasslv 2"
 	# Patch Messenger:
 	get_apk "com.facebook.orca" "messenger-beta-arm64-v8a" "apk" "arm64-v8a" "nodpi" "Android 9.0+"
-	npatch "messenger-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "messenger-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" " --sigbypasslv 2"
 }
 3() {
 	NPatch_dl
 	patch_dl
 	# Patch Instagram:
 	get_apk "com.instagram.android" "instagram-beta-arm64-v8a" "bundle" "arm64-v8a" "120-640dpi"  "Android 9.0+"
-	npatch "instagram-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "instagram-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" " --sigbypasslv 2"
 	# Patch Thread:
 	get_apk "com.instagram.barcelona" "threads-beta-arm64-v8a" "bundle" "arm64-v8a" "320-480dpi" "Android 9.0+"
-	npatch "threads-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "threads-beta-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh" " --sigbypasslv 2"
 }
 case "$1" in
     1)
